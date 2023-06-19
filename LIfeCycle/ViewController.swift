f66c935b8814ec5bd69eab6014ccb25dc40afb63
Dixon A <dixona2023@icloud.com>
2023-06-19 11:10:09 +0530

    "Initial commit"

--- LIfeCycle/ViewController.swift
+++ LIfeCycle/ViewController.swift
@@ -1,19 +1,52 @@
-//
-//  ViewController.swift
-//  LIfeCycle
-//
-//  Created by Dixon A on 19/06/23.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+ //  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+ //      super.init(nibName: nil, bundle: nil)
+ //  }
+ //
+  //override func loadView() {
+  //    super.loadView()
+  //}
+    
     override func viewDidLoad() {
+        //func load in to memory
+        //only happens when the app is loaded for the first time
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // add subviews
+        //prompt for password
+        //anything after the view appear
+        //control something appearing and disappearing
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    deinit {
+
     }
 
 
 }
+
+
+
 
